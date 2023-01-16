145847fb359c2170999b50144c25b2464f1a9678
Andrew Perkins <andrewperk@gmail.com>
2023-01-15 19:32:11 -0800

    First commmit

---
+++ AndrewsCookery/AndrewsCookeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndrewsCookeryApp.swift
+//  AndrewsCookery
+//
+//  Created by Andrew L Perkins on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AndrewsCookeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndrewsCookery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AndrewsCookery
+//
+//  Created by Andrew L Perkins on 1/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
