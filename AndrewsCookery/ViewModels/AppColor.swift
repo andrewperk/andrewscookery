9d1240ea2ba1c2b72166554ea55bb12e2d353b93
Andrew Perkins <andrewperk@gmail.com>
2023-01-18 15:08:22 -0800

    Created color theme

---
+++ AndrewsCookery/ViewModels/AppColor.swift
@@ -0,0 +1,27 @@
+//
+//  AppColor.swift
+//  AndrewsCookery
+//
+//  Created by Andrew L Perkins on 1/18/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppColor {
+    static let background: Color = Color(
+        .sRGB,
+        red: 228/255,
+        green: 235/255,
+        blue: 250/255,
+        opacity: 1
+    )
+    
+    static let foreground: Color = Color(
+        .sRGB,
+        red: 118/255,
+        green: 119/255,
+        blue: 231/255,
+        opacity: 1
+    )
+}
